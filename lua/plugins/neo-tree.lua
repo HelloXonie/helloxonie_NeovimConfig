e373e409591431f91207861e2653f010fc8490fc
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-26 10:32:16 -0700

    changed reveal to toggle

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
       "nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+		vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
 	end
 }
  
